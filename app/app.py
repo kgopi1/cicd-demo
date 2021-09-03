cecb825f34d0ac11bf7a013a38c518a18034d5ae
MOKSHI <k.gopi1@gmail.com>
2021-09-03 16:50:04 +0100

    added the env in python

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hey Gopi!, we Flask in a Docker container!'
+    env = os.environ["env"]
+    return f'Hey Gopi!, we Flask in a {env} Docker container!'
 
 
 if __name__ == '__main__':
